13f97a012529044aa800f2cce0dade3d9c3daa5a
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-01-17 20:26:11 +0100

    Move toolbar to side nav menu

--- src/CashFlow.Host/ClientApp/src/app/material.module.ts
+++ src/CashFlow.Host/ClientApp/src/app/material.module.ts
@@ -4,7 +4,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -16,7 +18,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     MatSelectModule,
+    MatSidenavModule,
     MatTableModule,
     MatTabsModule,
     MatToolbarModule
