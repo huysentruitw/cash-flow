6bd087539b98420296edbc5097b9d982dbb986dd
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-01-19 23:01:08 +0100

    Expose DateCreated for FinancialYear model

--- src/CashFlow.GraphApi/Schema/OutputTypes/FinancialYear.cs
+++ src/CashFlow.GraphApi/Schema/OutputTypes/FinancialYear.cs
@@ -7,5 +7,7 @@ namespace CashFlow.GraphApi.Schema
         public Guid Id { get; set; }
 
         public string Name { get; set; }
+
+        public DateTimeOffset DateCreated { get; set; }
     }
 }
