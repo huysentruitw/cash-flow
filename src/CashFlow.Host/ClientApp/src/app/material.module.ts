841846f3f60ad8ca747b8d1d56cb73398d165d50
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-01-26 23:03:19 +0100

    Add material radio stuff

--- src/CashFlow.Host/ClientApp/src/app/material.module.ts
+++ src/CashFlow.Host/ClientApp/src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
@@ -20,6 +21,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatRadioModule,
     MatSelectModule,
     MatSidenavModule,
     MatTableModule,
