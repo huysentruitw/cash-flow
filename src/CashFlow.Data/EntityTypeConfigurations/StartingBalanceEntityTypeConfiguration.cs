30e10e57939c6b2e511650da2eba55e8bb26b24d
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-01-26 16:20:48 +0100

    Add EntityTypeConfiguration for StartingBalance table

---
+++ src/CashFlow.Data/EntityTypeConfigurations/StartingBalanceEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+using CashFlow.Data.Abstractions.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CashFlow.Data.EntityTypeConfigurations
+{
+    internal sealed class StartingBalanceEntityTypeConfiguration : IEntityTypeConfiguration<StartingBalance>
+    {
+        public void Configure(EntityTypeBuilder<StartingBalance> builder)
+        {
+            builder.HasKey(x => new { x.FinancialYearId, x.AccountId });
+        }
+    }
+}
