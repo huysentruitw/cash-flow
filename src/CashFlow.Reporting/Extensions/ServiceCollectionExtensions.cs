20a6f6a10bec1d37a607e76f0c373d83cbc94696
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-01-29 19:01:30 +0100

    Add empty projects for housing reporting related stuff

---
+++ src/CashFlow.Reporting/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddReporting(this IServiceCollection services)
+    {
+        return services;
+    }
+}
