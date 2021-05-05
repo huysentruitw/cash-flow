3975adb7f19f3e766dea640d78dc25bd5ad97c9a
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2021-05-05 21:18:29 +0200

    Remove unused code

--- src/CashFlow.Enums/AccountType.cs
+++ src/CashFlow.Enums/AccountType.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Linq.Expressions;
-
 namespace CashFlow.Enums
 {
     public enum AccountType
@@ -10,29 +7,4 @@ namespace CashFlow.Enums
         CurrentAccount = 2,
         SavingsAccount = 3
     }
-
-    //public class Account
-    //{
-    //    public string Name { get; set; }
-        
-    //    public AccountType AccountType { get; set; }
-    //}
-
-    //public abstract class View<T>
-    //{
-    //    public abstract void ConfigureView();
-
-    //    protected object ForField(Expression<Func<T, object>> expression)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
-
-    //public class AccountView : View<Account>
-    //{
-    //    public override void ConfigureView()
-    //    {
-    //        ForField(x => x.AccountType).AsDropDown();
-    //    }
-    //}
 }
